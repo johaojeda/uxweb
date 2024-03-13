9f97774bdf90a821d9a1a5c19780213593b54682
Johana Ojeda <j.ojedaa@uniandes.edu.co>
2024-03-13 02:22:19 -0500

    Actualización navegacion pantallas

--- alarmatic-home/src/app/app-routing.module.ts
+++ alarmatic-home/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { RegistroComponent } from './registro/registro.component';
+import { HomeComponent } from './home/home.component';
+import { AnadirInvitadoComponent } from './anadir-invitado/anadir-invitado.component';
+import { RutinasComponent } from './rutinas/rutinas.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'registro', component: RegistroComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'anadir-invitado', component: AnadirInvitadoComponent },
+  { path: 'rutinas', component: RutinasComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
